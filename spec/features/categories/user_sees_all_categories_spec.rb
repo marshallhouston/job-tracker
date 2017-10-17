654d3cc699291af92aa5f55c7995b30bd8b079eb
Marshall Houston <marshall.houston@gmail.com>
2017-10-16 18:56:25 -0600

    remove save_and_open_page

--- spec/features/categories/user_sees_all_categories_spec.rb
+++ spec/features/categories/user_sees_all_categories_spec.rb
@@ -6,7 +6,6 @@ describe "user sees all categories" do
     Category.create!(title: 'Sports')
 
     visit categories_path
-    save_and_open_page
 
     expect(page).to have_content('Software')
     expect(page).to have_content('Sports')

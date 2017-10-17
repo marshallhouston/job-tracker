804ec7dc77956d0820b00819acbff0cf0b3e70ea
Marshall Houston <marshall.houston@gmail.com>
2017-10-16 18:33:34 -0600

    add category index with all categories

---
+++ spec/features/categories/user_sees_all_categories_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "user sees all categories" do
+  scenario "sees all categories" do
+    Category.create!(title: 'Software')
+    Category.create!(title: 'Sports')
+
+    visit categories_path
+
+    expect(page).to have_content('Software')
+    expect(page).to have_content('Sports')
+  end
+end
